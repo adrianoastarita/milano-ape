c8c739f78e588d98441bd1af801c161927abc09e
adrianoastarita <66206109+adrianoastarita@users.noreply.github.com>
2026-07-05 20:11:47 +0000

    wip..

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.render("index");
 });
+
+app.get("/preview", (req,res) => {
+    res.render("preview");
+});
+
 app.listen(PORT, () => {
     console.log(`Server avviato sulla porta ${PORT}`);
 });
